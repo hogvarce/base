6c2e6f96aaaa1987558f2f5412c3e0cdc35dc8f9
hogvarce <hogvarce@gmail.com>
2015-10-05 21:45:27 +0300

    add function in api controller

--- modules/api/controllers/GoodsController.php
+++ modules/api/controllers/GoodsController.php
@@ -2,10 +2,33 @@
 
 namespace app\modules\api\controllers;
 
-use yii\web\Controller;
+use app\models\Goods;
 use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
 
 class GoodsController extends ActiveController
 {
     public $modelClass = 'app\models\Goods';
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['index']);
+
+        return $actions;
+    }
+    public function actionIndex($filter = null, $sort = 'id', $sortMethod = SORT_ASC)
+    {
+        $query = Goods::find();
+        if ( $filter ) {
+            $filter = json_decode($filter, true);
+            $query  = $query->andWhere($filter);
+        }
+        return new ActiveDataProvider([
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+            'query' => $query,
+            'sort' => ['defaultOrder' => [$sort => $sortMethod]],
+        ]);
+    }
 }
