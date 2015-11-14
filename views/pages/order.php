18f87ab9471ae962b16a62ca509035bc650a36b6
hogvarce <hogvarce@gmail.com>
2015-11-15 00:03:42 +0300

    change order page add delivery self

--- views/pages/order.php
+++ views/pages/order.php
@@ -19,6 +19,7 @@
             <?= $form->field($model, 'customer_name')->input([]) ?>
             <?= $form->field($model, 'customer_phone')->input('tel', ['required' => true]) ?>
             <?= $form->field($model, 'customer_email')->input('email', ['required' => true]) ?>
+            <label>Самовывоз <input id="selfDelivery" name="self-delivery" type="checkbox"></label>
             <?= $form->field($model, 'order_address')->input([]) ?>
             <div id="map" data-x="59.7459481" data-y="30.0861305"></div>
             <?= $form->field($model, 'comments')->textArea(['rows' => 6]) ?>
@@ -49,6 +50,6 @@
         sumDiscount = ( <?= $delivery[0]->sum_delivery ?> > sum ) ? <?= $delivery[0]->cost_delivery ?> : 0;
         sum += sumDiscount;
      %>
-    <p><strong>Доставка: <%= (sumDiscount > 0) ? sumDiscount  : "бесплатно" %> <%= (sumDiscount > 0) ? declOfNum(Math.round(sumDiscount), ["рубль", "рубля", "рублей"]) : '' %></strong></p>
-    <h4>Итого: <%= sum %> <%=  declOfNum(Math.round(sum), ["рубль", "рубля", "рублей"]) %></h4>
+    <p id="sumDisc"><strong>Доставка: <span><%= (sumDiscount > 0) ? sumDiscount  : 0 %></span> <%= (sumDiscount > 0) ? declOfNum(Math.round(sumDiscount), ["рубль", "рубля", "рублей"]) : '' %></strong></p>
+    <h4>Итого: <span id="Sum"><%= sum %></span> <%=  declOfNum(Math.round(sum), ["рубль", "рубля", "рублей"]) %></h4>
 </script>
--- web/js/views/order.js
+++ web/js/views/order.js
@@ -10,15 +10,34 @@ app.order = Backbone.View.extend({
     render: function(){
         this.$el.html( this.template( this.model ) );
 
-            var name  = $('#customers-customer_name'),
-                phone = $('#customers-customer_phone'),
-                email = $('#customers-customer_email');
+            var name  = $('#customers-customer_name') || '',
+                phone = $('#customers-customer_phone') || '',
+                email = $('#customers-customer_email') || '';
 
-            var customer = JSON.parse(localStorage.getItem('customer'));
-            
+            var customer = JSON.parse(localStorage.getItem('customer') || '[]');
+
                 name.val(customer.name);
                 phone.val(customer.phone);
                 email.val(customer.email);
+
+            $('#selfDelivery').change(function(){
+                var sum = parseFloat($('#Sum').text());
+                if ( $(this).is(':checked') ) {
+                    $('.field-customers-order_address').stop(1,0).slideUp();
+                    $('#map').stop(1,0).slideUp();
+                    $('#sumDisc').stop(1,0).slideUp();
+                    $('#sumDisc strong > span').text(0);
+                    sum -= sumDiscount;
+                    $('#Sum').text(sum);
+                } else {
+                    $('.field-customers-order_address').stop(1,0).slideDown();
+                    $('#map').stop(1,0).slideDown();
+                    $('#sumDisc').stop(1,0).slideDown();
+                    $('#sumDisc strong > span').text(sumDiscount);
+                    sum += sumDiscount;
+                    $('#Sum').text(sum);
+                }
+            });
 
         return this;
     }
